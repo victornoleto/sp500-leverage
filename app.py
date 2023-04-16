53cb0220a9bc577f38b262dc1b99418ef0f6629f
Victor Noleto <noletovasco@gmail.com>
2023-04-16 19:14:05 -0300

    ER calculation and log y-axes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plotly.express as px
 
+expense_ratio = 1
+
 def calculate_leverage(data, leverage):
 
 	leverage_data = []
@@ -21,7 +23,15 @@ def calculate_leverage(data, leverage):
 
 			leverage_value = last_leverage_value * (1 + leverarage_appreciation)
 
-			if leverage_value < 0:
+			# I am not sure if the ER calculation is correct
+			if index % 365 == 0:
+
+				profit = leverage_value - leverage_data[index-365]
+
+				if profit > 0:
+					leverage_value -= (profit * (expense_ratio / 100))
+
+			if leverage_value < 0.1:
 				leverage_value = 0
 
 		leverage_data.append(leverage_value)
@@ -38,11 +48,13 @@ sp500['Leverage_1.25x'] = calculate_leverage(close_values, 1.25)
 sp500['Leverage_1.5x'] = calculate_leverage(close_values, 1.5)
 sp500['Leverage_2x'] = calculate_leverage(close_values, 2)
 sp500['Leverage_3x'] = calculate_leverage(close_values, 3)
+sp500['Leverage_5x'] = calculate_leverage(close_values, 5)
 
 fig = px.line(
 	sp500,
 	x='Date',
-	y=['Close', 'Leverage_1.25x', 'Leverage_1.5x', 'Leverage_2x', 'Leverage_3x']
+	y=['Close', 'Leverage_1.25x', 'Leverage_1.5x', 'Leverage_2x', 'Leverage_3x', 'Leverage_5x'],
+	log_y=True
 )
 
 fig.show()
